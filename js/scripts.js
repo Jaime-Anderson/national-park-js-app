981e2a52af4093e4d2d48ff778a3e47873bf2eb3
Jaime-Anderson <janderson5217@gmail.com>
2022-11-03 21:02:34 -0400

    Created and added objects to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-alert('Hello world');
-
-let favoriteFood = 'Chicken Pot Pie';
-document.write(favoriteFood);
-
+let pokemonList = [
+    {name: 'Charmander', height: 3, types: 'fire'},
+    {name:'Oddish', height: 1, types: ['grass','poison']},
+    {name: 'Cleffa', height: 2, types: 'fairy'},
+];
 
 
 
