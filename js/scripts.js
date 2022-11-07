49658d00ef560c4df7cd3373f719ac73fa0b493f
Jaime-Anderson <janderson5217@gmail.com>
2022-11-06 22:12:12 -0500

    Corrected spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
 let pokemonList = [
-    {name: 'Charmander', height: 3, types: 'fire'},
-    {name:'Oddish', height: 1, types: ['grass','poison']},
-    {name: 'Cleffa', height: 2, types: 'fairy'},
+    {
+        name: 'Charmander', 
+        height: 3, 
+        types: 'fire'
+    },
+    {
+        name:'Oddish', 
+        height: 1, 
+        types: ['grass','poison']
+    },
+    {
+        name: 'Cleffa', 
+        height: 2, 
+        types: 'fairy'
+    },
 ];
  
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height < 2) {
         document.write(pokemonList[i].name + ' (height: 1) ');
     }else if (pokemonList[i].height > 2) {
-        document.write(pokemonList[i].name + ' (height: 3) ');
+        document.write(pokemonList[i].name + ' (height: 3) -Wow! That\'s big! ');
     }else {
         document.write(pokemonList[i].name + ' (height: 2) ');
     }
