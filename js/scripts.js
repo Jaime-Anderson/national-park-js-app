6e4518ae6f40c42e172342c0e23dc36756b8505f
Jaime-Anderson <janderson5217@gmail.com>
2022-11-06 21:04:02 -0500

    Created for loop for each item in pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,13 @@ let pokemonList = [
     {name:'Oddish', height: 1, types: ['grass','poison']},
     {name: 'Cleffa', height: 2, types: 'fairy'},
 ];
-
-
-
-
+ 
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 2) {
+        document.write(pokemonList[i].name + " (height: 1)");
+    }else if (pokemonList[i].height > 2) {
+        document.write(pokemonList[i].name + " (height: 3)");
+    }else {
+        document.write(pokemonList[i].name + " (height: 2");
+    }
+}
